d496a0e4abc0dfc92b1cb2dfa10c931f546c86d8
Marek Michalski <marek.michalski@awesome-ind.com>
2020-10-23 16:58:54 +0200

    init

---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="navBar">
+    navbar
+  </nav>
+</template>
+
+<script lang="ts">
+import {Options, Vue} from 'vue-class-component';
+
+@Options({})
+export default class NavBar extends Vue {
+
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
